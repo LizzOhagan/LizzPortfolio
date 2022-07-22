5f1bbed802ee01679f9591ea097a6057a19a6f79
Elizabeth O'Hagan <elizabeth.ohagan@developersinstitute.co.nz>
2022-07-22 21:54:48 +1200

    edited App.js with name and blah blah for fun

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+// This is the page that the routes go on - the components are the pages that are the different routes - nb each of those can have their own routes as well
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>My name is Lizz</p>
+      <p>Elizabeth O'Hagan</p>
+      <p>Lizz ...... blah blah blah</p>
     </div>
   );
 }
